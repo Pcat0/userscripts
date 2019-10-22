beb49f441e31c35ab6d413ee67718df1f0b0a04b
Patrick Ambli <patrick.ambli@gmail.com>
2019-10-22 13:42:30 -0500

    Update MyserviceAdminTools.user.js

--- MyserviceAdminTools/MyserviceAdminTools.user.js
+++ MyserviceAdminTools/MyserviceAdminTools.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
-// @name         Myservice Admin Tools -Beta
+// @name         Myservice Admin Tools
 // @namespace    https://github.com/Pcat0
 // @homepageURL  https://github.com/Pcat0/Userscripts
 // @version      0.1.1
 // @description  Adding features to the Myservice Admin, that really should already exist.
 // @author       Patrick Ambli
 // @run-at       document-idle
-// @updateURL    https://raw.githubusercontent.com/Pcat0/Userscripts/beta/MyserviceAdminTools/MyserviceAdminTools.user.js
-// @downloadURL  https://raw.githubusercontent.com/Pcat0/Userscripts/beta/MyserviceAdminTools/MyserviceAdminTools.user.js
+// @updateURL    https://raw.githubusercontent.com/Pcat0/Userscripts/master/MyserviceAdminTools/MyserviceAdminTools.user.js
+// @downloadURL  https://raw.githubusercontent.com/Pcat0/Userscripts/master/MyserviceAdminTools/MyserviceAdminTools.user.js
 // @supportURL   https://github.com/Pcat0-SE/Userscripts/issues
 // @match        *://*.govplatform.com/admin/MyServices/service/edit/*
 // @grant        none
