fe22401670d7ea2c00527f1ef362474c17695f37
Patrick Ambli <patrick.ambli@gmail.com>
2019-10-22 12:34:36 -0500

    Update MyserviceAdminTools.user.js

--- MyserviceAdminTools/MyserviceAdminTools.user.js
+++ MyserviceAdminTools/MyserviceAdminTools.user.js
@@ -7,7 +7,6 @@
 // @author       Patrick Ambli
 // @run-at       document-idle
 // @updateURL    https://raw.githubusercontent.com/Pcat0/Userscripts/master/MyserviceAdminTools/MyserviceAdminTools.user.js
-// @downloadURL  https://raw.githubusercontent.com/Pcat0/Userscripts/master/MyserviceAdminTools/MyserviceAdminTools.user.js
 // @supportURL   https://github.com/Pcat0-SE/Userscripts/issues
 // @match        *://*.govplatform.com/admin/MyServices/service/edit/*
 // @grant        none
