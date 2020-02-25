e78ced7be8e1a7bcf3bd16c8b56ae280d3116cb1
Patrick Ambli <patrick.ambli@gmail.com>
2020-02-25 10:53:01 -0600

    Create dev-site-name-watermark.user.js

---
+++ dev-site-name-watermark/dev-site-name-watermark.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name         Dev Site Name Watermark
+// @namespace    https://github.com/Pcat0
+// @homepageURL  https://github.com/Pcat0/Userscripts
+// @version      0.1.2
+// @description  Adds a site name watermark to the Dev sites. This allows for more easy ideation for what site one is working on.
+// @author       Patrick Ambli
+// @run-at       document-idle
+// @updateURL
+// @downloadURL
+// @supportURL
+// @match        *://*.govplatform.com/*
+// @grant        GM_addStyle
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    var {siteName} = window.location.hostname.match(/^[^.]+\.(?<siteName>[^.]+)\./).groups;
+    document.body.insertAdjacentHTML('afterbegin', `<div style="opacity: 0.3; font-size: 200px; position: fixed; right: 0px; bottom: 0px; pointer-events: none; font-family: Arial, 'Open Sans', sans-serif; z-index: 9999999; ">${siteName}</div>`)
+    // Your code here...
+})();
