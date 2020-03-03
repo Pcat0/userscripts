02b82805a484e35dbf2bc2997108a9ad6064daa9
Patrick Ambli <patrick.ambli@gmail.com>
2020-03-03 14:54:32 -0600

    1.0!
    Added Time tracking
    Improved the UI 
    Fixed warping bug
    Fixed the disappearing counter bug

--- KanbanBoardIssueCounter/KanbanBoardIssueCounter.user.js
+++ KanbanBoardIssueCounter/KanbanBoardIssueCounter.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Kanban board issue counter
-// @version      0.2
+// @version      1.0
 // @description  Add issue counters to jira's kanban board columns.
 // @author       Patrick Ambli
 //
@@ -16,16 +16,54 @@
 // @grant        none
 // ==/UserScript==
 
-(function(){
-    new MutationObserver(function(mutationList){
-        if(mutationList[0].removedNodes.length > 0){
-            for (let header of document.querySelector("#ghx-column-header-group>ul").children) {
-                let count = document.querySelectorAll(`li[data-column-id='${header.getAttribute("data-id")}']>div[data-issue-id]`).length;
-                header.getElementsByTagName("h2")[0].innerHTML += ` (${count})`;
+(function() {
+    const TIME_UNITS = {
+        'w': 2400,
+        'd': 480,
+        'h': 60,
+        'm': 1,
+    }
+    function parseTimeStr(timeStr) {
+        return timeStr.split(" ").reduce((acc, cur) => acc + TIME_UNITS[cur.slice(-1)] * cur.slice(0, -1), 0) || 0;
+    }
+    function formatTime(minutes, units, maxUnits = Infinity, decimalStep = .25) {
+        var output = []; 
+        for (const unit of units) {
+            let quotient = minutes / TIME_UNITS[unit];
+            if (!Number.isInteger(quotient / decimalStep)){ // Check if number is a multiple of [decimalStep]
+                if(output.length + 1 >= maxUnits || units[units.length-1] == unit) { // Check if this is the last unit 
+                    quotient = (Math.round(quotient / decimalStep) * decimalStep); //round to the nearest [decimalStep]
+                } else {
+                    quotient |= 0; //floor quotient
+                }
             }
-            //Fix Kanban Board Header 
-            var offset = document.getElementById('ghx-column-header-group').offsetHeight;
-            document.getElementById('ghx-pool').style.paddingTop = `${offset}px`;
+            if(quotient > 0){
+                minutes -= quotient * TIME_UNITS[unit];
+                if (output.push(`${quotient}${unit}`) >= maxUnits){
+                    break;
+                }
+            }
+            
+            
         }
-     }).observe(document.getElementsByClassName("ghx-throbber")[0], {childList: true});
+
+        return output.join(' ');
+    }
+    AJS.$(GH).bind('workModeUIReady', function() { //Hijack the GreenHopper event system to know when the kanban board has been refeshed. 
+        console.log(`WorkModeUIReady`);
+        for (let header of document.querySelector("#ghx-column-header-group>ul").children) {
+            let column = document.querySelectorAll(`li[data-column-id='${header.getAttribute("data-id")}']>div[data-issue-id]`);
+            let totalTimeLeft = 0;
+            for (let issue of column) {
+                let timeLeftStr = issue.querySelector(".ghx-corner aui-badge").innerText;
+                totalTimeLeft += parseTimeStr(timeLeftStr);
+            }
+
+            header.innerHTML += `<aui-badge style="float: right;" title='Remaining Time Estimate'>${formatTime(totalTimeLeft, ['d', 'h', 'm'], 2)}</aui-badge><br>
+            <span class="aui-lozenge aui-lozenge-subtle" title="Total number of tickets">${column.length} Tickets</span>`;
+        }
+        //Uppdate kanban board offset to fix any header overflow.
+        var offset = document.getElementById('ghx-column-header-group').offsetHeight;
+        document.getElementById('ghx-pool').style.paddingTop = `${offset}px`;
+    });
 })();
